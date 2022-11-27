25cd61a90049585e06c0eab2eb3f81548ba02666
abhinav-18max <abhinav200218@gmail.com>
2022-11-28 02:37:40 +0530

    razorpay

--- api/payments/payment.js
+++ api/payments/payment.js
@@ -15,32 +15,50 @@ const payments = {
 
       console.log(digest, req.headers["x-razorpay-signature"]);
 
-      if (digest === req.headers["x-razorpay-signature"]) {
-        console.log("request is legit");
-
-        if (!req.body.payload.payment.entity.captured) {
-          return res.status(400).send({
-            sucess: false,
-            msg: "payment failed",
-          });
-        }
-
-        if (req.body.payload.payment.entity.amount_paid !== 200) {
-          return res.status(400).send({
-            sucess: false,
-            msg: "payment failed, amount paid is not 150 rs",
-          });
-        }
-
-        part.findOneAndUpdate(
-          { razorpayorderid: req.body.payload.payment.entity.order_id },
-          { paymentstatus: "payed" }
-        );
-        return res.send(200).json({ message: " Not Verified your payment" });
+      if (digest !== req.headers["x-razorpay-signature"]) {
+        return res.status(200).json({ msg: "Transaction not legit!" });
       }
+
+      console.log("request is legit");
+
+      if (!req.body.payload.payment.entity.captured) {
+        await part.findOneAndUpdate(
+          { razorpayorderid: req.body.payload.payment.entity.order_id },
+          { paymentstatus: "failed" }
+        );
+
+        return res.status(200).json({});
+      }
+
+      if (req.body.payload.payment.entity.amount_paid !== 200) {
+        await part.findOneAndUpdate(
+          { razorpayorderid: req.body.payload.payment.entity.order_id },
+          { paymentstatus: "failed" }
+        );
+
+        return res.status(200).send({
+          sucess: false,
+          msg: "payment failed, amount paid is not 150 rs",
+        });
+      }
+
+      console.log("Payment Confirmed");
+
+      const updatedOrder = await part.findOneAndUpdate(
+        { razorpayorderid: req.body.payload.payment.entity.order_id },
+        { paymentstatus: "payed" },
+        {
+          new: true,
+        }
+      );
+
+      console.log("Database Updated");
+      console.log(`\nUpdated Order\n\n${updatedOrder}\n\n`);
+
+      return res.send(200).json({ message: "Verified your payment" });
     } catch (error) {
       console.log(error);
-      return res.status(500).json({ message: "Internal server Error!" });
+      return res.status(200).json({ message: "Internal server Error!" });
     }
   },
 };
